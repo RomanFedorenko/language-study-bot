5ab2115e82cc3d98ffed7c0240d583e720d2a54b
RomanFedorenko <frank.lamo@ukr.net>
2020-06-23 00:46:42 +0300

    Initial commit

---
+++ web/src/main/java/com/fedorenko/bot/LanguageStudyApiApplication.java
@@ -0,0 +1,20 @@
+package com.fedorenko.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class LanguageStudyApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LanguageStudyApiApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication sa = new SpringApplication(LanguageStudyApiApplication.class);
+        sa.run(args);
+    }
+}
